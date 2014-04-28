b37fd143133485bb4f5b4e016cf4dec0a8b347fa
Alejandro Esquiva Rodríguez <alexbigastro@gmail.com>
2014-04-28 22:01:39 +0200

    Version 1.0 finished

--- TO-DO.php
+++
@@ -1,9 +0,0 @@
-<meta charset="utf-8">
-TO-DO:<br><br>
-
--Poder elegir en la tabla el número de elementos a mostrar<br>
--Poder Elegir el orden por campo y si es ascendente o descendente<br>
--Poder guardar los enlces personalizados que hemos creado<br>
--Poder elegir aquellos enlaces privado y publico<br>
--Poder Encriptar la información<br>
--Usar un usuario y contraseña para entrar a la aplicación<br>
--- test.php
+++
@@ -1,4 +0,0 @@
-<?php
-include_once 'inc/functions.php';
-$blacklist = new BlackList();
-$blacklist->createItem("G","badge","id_badge");
