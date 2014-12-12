4c12bfca7a2595af6b44cb6144e090a3b202bdfe
Alejandro Esquiva <alexbigastro@gmail.com>
2014-12-12 11:43:06 +0000

    Nothing special

--- AutomaticApiRest/examples/Python/AARConnector/test.py
+++ AutomaticApiRest/examples/Python/AARConnector/test.py
@@ -1,4 +1,4 @@
-__author__ = 'Alesandro.Esquiva'
+__author__ = 'Alejandro Esquiva Rodriguez'
 
 from AARConnector import *
 
@@ -10,7 +10,7 @@ AAR = AARConnector(url="http://automaticapirest.info/demo/getData.php?t=Country&
 ##Create instance via parameters
 AAR = AARConnector(domain="http://automaticapirest.info/demo/",table="Country",columns="Name",orderby="Name",limit="10",where="Name:'Albania'")
 
-#Get all the data
+#Get all the json
 jsondata = AAR.getJson()
 '''
 print(jsondata)
@@ -18,8 +18,25 @@ print(jsondata)
 {'dbInfo': ['Code', 'Name'], 'data': [{'1': 'Aruba', 'Code': 'ABW', '0': 'ABW', 'Name': 'Aruba'}, {'1': 'Afghanistan', 'Code': 'AFG', '0': 'AFG', 'Name': 'Afghanistan'}, {'1': 'Angola', 'Code': 'AGO', '0': 'AGO', 'Name': 'Angola'}, {'1': 'Anguilla', 'Code': 'AIA', '0': 'AIA', 'Name': 'Anguilla'}, {'1': 'Albania', 'Code': 'ALB', '0': 'ALB', 'Name': 'Albania'}]}
 '''
 
+#Get all the data
+data = AAR.getData()
+
+#Get query info
+dbinfo = AAR.getDBInfo()
+
+#Output a specific data
+
+#Name in the first row
+name = data[0]["Name"]
+#OR
+name = data[0]["0"]
+
+print(name)
+
+
 #Print query info
 AAR.printDBInfo()
+
 
 '''
 [
@@ -65,3 +82,5 @@ AAR.printData()
  }
 ]
 '''
+
+
