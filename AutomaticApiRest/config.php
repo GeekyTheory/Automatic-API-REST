87ca41d398ab2256395d33850ea6289e19ea117f
Dario Arias <dariogabrielarias@gmail.com>
2015-05-28 11:29:08 -0300

    - Correccion de espacion en archivos, daba error en version windows APACHE - Uso de .htaccess (Requerido activar en apache) - intento de RESTFUL API - Archivo acciones para Delte Post y Update

--- AutomaticApiRest/config.php
+++ AutomaticApiRest/config.php
@@ -1,8 +1,13 @@
 <?php
+//pph.ini
+//@ini_set('memory_limit','256M');
+
+define("DIRAPI","/Automatic-API-REST/AutomaticApiRest/");
+
 define("SERVER","localhost");
-define("USER","root");
-define("PASS","");
-define("DB","world");
+define("USER","INFORMATICA");
+define("PASS","4201-3222/7760/4780");
+define("DB","turnos");
 
 define("USERADMIN","admin");
 define("PASSADMIN","admin");
